c2ae6028df619bf92f87331529ad0d871125738b
httpoz <oscar.mwana@gmail.com>
2025-02-07 04:25:43 +0100

    wip structured formatting

--- go.mod
+++ go.mod
@@ -2,11 +2,21 @@ module github.com/httpoz/faafo-go-cli
 
 go 1.23.4
 
-require github.com/openai/openai-go v0.1.0-alpha.51
+require (
+	github.com/invopop/jsonschema v0.13.0
+	github.com/openai/openai-go v0.1.0-alpha.55
+	github.com/sirupsen/logrus v1.9.3
+)
 
 require (
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/tidwall/gjson v1.18.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/tidwall/sjson v1.2.5 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,56 +2,108 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"log"
 	"os"
 
+	"github.com/sirupsen/logrus"
+
+	"github.com/invopop/jsonschema"
 	"github.com/openai/openai-go"
 	"github.com/openai/openai-go/option"
 )
 
 const (
-	llama32      openai.ChatModel = "llama3.2"
-	deepseek     openai.ChatModel = "deepseek-r1:1.5b"
-	systemPrompt string           = "files/system-prompt.md"
-	userMessage  string           = "files/sample-api.json"
+	llama32          openai.ChatModel = "llama3.2"
+	deepseek         openai.ChatModel = "deepseek-r1"
+	systemPromptFile string           = "files/system-prompt.txt"
+	userMessageFile  string           = "files/sample-api.json"
+)
+
+type LLMStructuredResponse struct {
+	Status        string `json:"status" jsonschema_description:"Indicates whether the OpenAPI specification is 'compliant' (valid) or 'fixed' (errors corrected)."`
+	Message       string `json:"message,omitempty" jsonschema_description:"A brief summary describing any modifications made to fix structural or syntax errors in the OpenAPI specification. This field is omitted if no fixes were needed."`
+	CorrectedSpec string `json:"corrected_spec,omitempty" jsonschema_description:"The corrected OpenAPI specification as a JSON object. This field is only present if errors were found and fixed."`
+}
+
+var (
+	log                            = logrus.New()
+	ValidatedOpenAPISchemaResponse = GenerateSchema[LLMStructuredResponse]()
 )
 
 func main() {
 	ctx := context.Background()
+	log.SetFormatter(&logrus.JSONFormatter{})
 
-	jsonFile, error := readFileContents("files/sample-api.json")
+	jsonFile, error := readFileContents(userMessageFile)
 	if error != nil {
-		log.Fatalf("failed to read JSON file: %v", error)
+		log.Fatal("failed to read JSON file: ", error)
 	}
 
-	systemPrompt, error := readFileContents("files/system-prompt.md")
+	systemPrompt, error := readFileContents(systemPromptFile)
 	if error != nil {
-		log.Fatalf("failed to read system prompt file: %v", error)
+		log.Fatal("failed to read system prompt file: ", error)
 	}
 
 	client := openai.NewClient(option.WithBaseURL("http://localhost:11434/v1/"))
 
-	response, err := chat(ctx, client, llama32, systemPrompt, jsonFile)
+	response, err := chat(ctx, client, deepseek, systemPrompt, jsonFile)
 	if err != nil {
-		log.Fatalf("chat request failed: %v", err)
+		log.Fatal("chat request failed: ", err)
 	}
 
-	fmt.Println("Chat response:", response)
+	if response.Status == "fixed" {
+		// Write response to file
+		file, err := os.Create("files/fixed-spec.json")
+		if err != nil {
+			log.Fatal("failed to create file: ", err)
+		}
+		defer file.Close()
+
+		_, err = file.WriteString(response.CorrectedSpec)
+		if err != nil {
+			log.Fatal("failed to write to file: ", err)
+		}
+	}
+
+	fmt.Println(response.Status, " ", response.Message)
 }
 
-func chat(ctx context.Context, client *openai.Client, model openai.ChatModel, systemPrompt, userMessage string) (string, error) {
-	chatCompletion, err := client.Chat.Completions.New(ctx, openai.ChatCompletionNewParams{
+func chat(ctx context.Context, client *openai.Client, model openai.ChatModel, systemPrompt, userMessage string) (*LLMStructuredResponse, error) {
+	schemaParam := openai.ResponseFormatJSONSchemaJSONSchemaParam{
+		Name:        openai.F("validated open api spec"),
+		Description: openai.F("Structured response from LLM for OpenAPI validation and correction"),
+		Schema:      openai.F(ValidatedOpenAPISchemaResponse),
+		Strict:      openai.Bool(true),
+	}
+
+	chat, err := client.Chat.Completions.New(ctx, openai.ChatCompletionNewParams{
 		Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
 			openai.SystemMessage(systemPrompt),
 			openai.UserMessage(userMessage),
 		}),
 		Model: openai.F(model),
+		ResponseFormat: openai.F[openai.ChatCompletionNewParamsResponseFormatUnion](
+			openai.ResponseFormatJSONSchemaParam{
+				Type:       openai.F(openai.ResponseFormatJSONSchemaTypeJSONSchema),
+				JSONSchema: openai.F(schemaParam),
+			},
+		),
 	})
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return chatCompletion.Choices[0].Message.Content, nil
+
+	llmResponse := chat.Choices[0].Message.Content
+	log.Info(llmResponse)
+
+	var structuredResponse LLMStructuredResponse
+	err = json.Unmarshal([]byte(llmResponse), &structuredResponse)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing structured JSON: %w", err)
+	}
+
+	return &structuredResponse, nil
 }
 
 func readFileContents(path string) (string, error) {
@@ -61,4 +113,16 @@ func readFileContents(path string) (string, error) {
 	}
 
 	return string(f), nil
+}
+
+func GenerateSchema[T any]() interface{} {
+	// Structured Outputs uses a subset of JSON schema
+	// These flags are necessary to comply with the subset
+	reflector := jsonschema.Reflector{
+		AllowAdditionalProperties: false,
+		DoNotReference:            true,
+	}
+	var v T
+	schema := reflector.Reflect(v)
+	return schema
 }
